727ac6f03b1d84c07461c3737c2038fd5c1c4980
HMartinez20 <32554944+HMartinez20@users.noreply.github.com>
2019-02-06 17:52:05 -0600

    Update fire.js

--- fire.js
+++ fire.js
@@ -3,8 +3,8 @@ var config = {
   apiKey: "AIzaSyBqm2UWSLit27_pLmEd6nn_WX41lzSRAYU",
   authDomain: "gobowling-55686.firebaseapp.com",
   databaseURL: "https://gobowling-55686.firebaseio.com",
-  projectId: "gobowling-55686",
   storageBucket: "gobowling-55686.appspot.com",
-  messagingSenderId: "523257812980"
 };
 firebase.initializeApp(config);
+
+var database = firebase.database();
